8fd25177f57386c8322007b340bad2e8b9804d1f
RominaMaita <u202213765@upc.edu.pe>
2024-09-03 13:23:51 -0500

    feat: added state and behavior to address value object.

---
+++ src/main/java/com/acme/shared/domain/model/value/objects/Address.java
@@ -0,0 +1,34 @@
+package com.acme.shared.domain.model.value.objects;
+
+public class Address {
+    private final String street;
+    private final String number;
+    private final String city;
+    private final String state;
+    private final String zipCode;
+    private final String country;
+
+    public Address(){
+        this.street="";
+        this.number="";
+        this.city="";
+        this.state="";
+        this.zipCode="";
+        this.country="";
+    }
+
+
+    public Address(String street, String number, String city, String state, String zipCode, String country) {
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+
+    /*añadiendo un metodo para juntar en un string los valores de Address*/
+    public String getAddressAsString(){
+        return String.format("%s, %s,%s,%s,%s,%s", street, number, city, state, zipCode, country);
+    }
+}
